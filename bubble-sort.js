479c7a43e4209d8fc4020dfa01efa3c16392ce23
Raul Veras <verasraul@gmail.com>
2022-03-20 11:33:36 -0400

    added bubble sorting algo code

--- bubble-sort.js
+++ bubble-sort.js
@@ -6,4 +6,19 @@
     // Return the sorted array
 
 
-    
+
+function bubbleSort(arr){
+    for(let i = 0; i < arr.length; i++){
+        for (let j = 0; j < arr. length; j++){
+            if (arr[j] > arr[j+1]){
+                // SWAP!
+                let temp = arr[j];
+                arr[j] = arr[j+1]
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(bubbleSort([37, 45,29,8]))
