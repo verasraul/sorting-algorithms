bf2799409a18f8f92963499f6341e3e42c7381e3
Raul Veras <verasraul@gmail.com>
2022-03-20 09:54:57 -0400

    added bubble sort pseudo code

---
+++ bubble-sort.js
@@ -0,0 +1,9 @@
+
+// BUBBLE-SORT PSEUDO-CODE:
+    // Start looping from with a variable called i the end of the array towards the beginning
+    // Start an inner loop with a variable called j from the beginning until i - 1
+    // If arrlj] is greater than arr[j+1], swap those two values!
+    // Return the sorted array
+
+
+    
